16ba075f2b0e99017e482ee695043ad37126a018
Mykola Nikolayev <nikolayev.mykola@gmail.com>
2023-05-07 16:00:27 -0400

    problem #8

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,4 @@
+import net.projecteuler.p8.LargestProductSeries
 import java.math.BigInteger
 
 fun main(args: Array<String>) {
@@ -5,6 +6,9 @@ fun main(args: Array<String>) {
 
 //    println("\nsum="+Multiple3and5.findMultiplesSum(1000))
 
-    println("\nsum="+Fibonacci.findSumOfEvenTerms(BigInteger("40"), BigInteger.ZERO, BigInteger.ONE, BigInteger.ZERO) )
+//    println("\nsum="+Fibonacci.findSumOfEvenTerms(BigInteger("40"), BigInteger.ZERO, BigInteger.ONE, BigInteger.ZERO) )
 
+    print(LargestProductSeries.getLargestProductInASeries())
+    // 23,514,624,000.
+    // 23 514 624 000
 }
